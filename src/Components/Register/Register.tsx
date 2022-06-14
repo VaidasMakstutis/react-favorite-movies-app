736c20679198b870950bed0e816a0b5fd7afb64e
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-06-14 17:04:14 +0300

    Submit registration form

--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { register, auth } from "../../services/authServices";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+export interface IUserData {
+  name: string;
+  email: string;
+  password: string;
+  prevState: null;
+}
 
 const Register = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+  
+  useEffect(()=>{
+    if(loading) return
+    if(user) navigate("/movies")
+  }, [user, loading, navigate])
+  
+  const submitHandler = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    if(!name) alert("Please enter your name")
+    register(name, email, password)
+  }
+
   return (
-    <Form className="mt-5">
+    <Form className="mt-5" onSubmit={submitHandler}>
       <h3 className="mb-3 text-center">Create your account</h3>
       <Form.Group className="mb-2">
-        <Form.Control type="text" name="name" placeholder="Your name" />
+        <Form.Control type="text" name="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-2">
-        <Form.Control type="email" name="email" placeholder="E-mail" />
+        <Form.Control type="email" name="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-2">
-        <Form.Control type="password" name="password" placeholder="Password" />
+        <Form.Control type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </Form.Group>
       <Button className="me-2" variant="primary" type="submit">
         Register
