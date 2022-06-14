ca07d9de08dc6524bcbefc13ddf3b493abc57cfc
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-06-14 21:54:12 +0300

    User info component

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,15 +1,14 @@
-import { SetStateAction } from 'react';
-import { IUserData } from '../Components/Register/Register';
 import firebase from '../firebaseConfig';
 
-export const getUserData = (user: firebase.User | null | undefined, setUser: { (value: SetStateAction<IUserData | null>): void; (arg0: firebase.firestore.DocumentData): void; }) => {
+export const getUserData = (user: firebase.User, 
+    setUserData: (value: any) => void) => {
     try {
         firebase
             .firestore()
             .collection('users')
             .where("uid", "==", user?.uid)
             .get()
-            .then((userData) => {setUser(userData.docs[0].data()) })
+            .then((userData) => {setUserData(userData.docs[0].data()) })
             
     }catch(error){
         console.log(error)
