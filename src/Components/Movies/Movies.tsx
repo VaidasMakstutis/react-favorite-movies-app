c553e157986480e3746e2d07a767ebab5a1e0ea0
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-06-16 13:07:58 +0300

    Show movies component

--- src/Components/Movies/Movies.tsx
+++ src/Components/Movies/Movies.tsx
@@ -3,17 +3,17 @@ import axios from "axios";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../services/authServices";
 import { useNavigate } from "react-router-dom";
+import ShowMovies from "./ShowMovies";
 
 export interface TMovie {
-  title: string;
-  year: number;
+  Title: string;
+  Year: number;
   imdbID: string;
-  type: string;
-  poster: string;
+  Type: string;
+  Poster: string;
 }
 
 const Movies = () => {
-
   const [movies, setMovies] = useState<TMovie[]>([]);
   const [user, error, loading] = useAuthState(auth);
   const navigate = useNavigate();
@@ -23,18 +23,22 @@ const Movies = () => {
   }, [user]);
 
   const getResponse = async () => {
-    await axios.get(`https://www.omdbapi.com/?s=Batman&apikey=887f9f42`)
-    .then(res => {
+    await axios.get(`https://www.omdbapi.com/?s=Batman&apikey=887f9f42`).then(res => {
       const data: TMovie[] = res.data.Search;
-      setMovies(data);
+      setMovies([...data]);
     });
-  }
+  };
 
   useEffect(() => {
     getResponse();
   }, []);
-  
-   return <h3 className="mt-5">Your Favorite Movies List</h3>;
+
+  return (
+    <>
+      <h4 className="mt-5">Your Favorite Movies List</h4>
+      <ShowMovies movies={movies} />
+    </>
+  );
 };
 
 export default Movies;
---
+++ src/Components/Movies/ShowMovies.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { TMovie } from "./Movies";
+
+interface IShowMoviesProps {
+  movies: TMovie[];
+}
+
+const ShowMovies = ({ movies }: IShowMoviesProps) => {
+  return (
+    <>
+      <div className="movies-container">
+        {movies &&
+          movies.map((movie, i) => {
+            return (
+              <Card key={i}>
+                <Card.Body>
+                    <p className="card-body-info">{movie.Title} {movie.Year}</p>
+                    <img src={movie.Poster} title={movie.Title} alt={movie.Title}  />
+                </Card.Body>
+              </Card>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+export default ShowMovies;
