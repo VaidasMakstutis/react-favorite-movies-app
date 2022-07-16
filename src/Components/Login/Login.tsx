1af4e91ff17d74fd47347ffaa188949bd7eee661
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-07-16 20:08:37 +0300

    ESLint configuration

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,38 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import { auth, signIn} from "../../services/authServices";
+import { auth, signIn } from "../../services/authServices";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { RouteKey } from "../../navigation/router";
 
 const Login = () => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
-  
-  const submitHandler:React.FormEventHandler<HTMLFormElement> = (event) => {
-      event.preventDefault();
-      signIn(email, password);
-  }
-  
-  useEffect(()=> {
-      if(loading) return
-      if(user) navigate(RouteKey.Movies)
-  }, [user, loading])
+
+  const submitHandler: React.FormEventHandler<HTMLFormElement> = event => {
+    event.preventDefault();
+    signIn(email, password);
+  };
+
+  useEffect(() => {
+    if (loading) return;
+    if (user) navigate(RouteKey.Movies);
+  }, [user, loading]);
 
   return (
     <>
       <Form className="mt-5" onSubmit={submitHandler}>
         <h3 className="mb-3 text-center">Login to your account</h3>
         <Form.Group className="mb-2">
-          <Form.Control type="email" name="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+          <Form.Control type="email" name="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} required />
         </Form.Group>
         <Form.Group className="mb-2">
-          <Form.Control type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
         </Form.Group>
-        <Button  className="mb-3" variant="primary" type="submit">
+        <Button className="mb-3" variant="primary" type="submit">
           Login
         </Button>
       </Form>
